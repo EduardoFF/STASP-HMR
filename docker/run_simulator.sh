b6e3b916b9513f219f06d3b3a7352f75e111dc9f
Eduardo Feo <eduardo@idsia.ch>
2020-07-10 12:01:33 +0300

    added run simulator from docker

---
+++ docker/run_simulator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Usage:  bash run_simulator.sh <output folder> instance-id agents
+# example: bash run_simulator.sh /home/eduardo/stasp/tests L10-R1-1 n4t2-1
+
+SHAREDVOL=$1
+docker run -it  --rm \
+       --env="DISPLAY" \
+       --env="QT_X11_NO_MITSHM=1" \
+       --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+       -v ${SHAREDVOL} \
+       --privileged \
+       --net="host"\
+       eduardofeo:swarmix-full \
+       /launch/simulator ${@:2} -ep ${SHAREDVOL}
