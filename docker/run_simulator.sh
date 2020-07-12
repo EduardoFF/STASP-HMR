2ab73d6573700079819940ab370f0cf61c7ec7a5
Eduardo Feo <eduardo@idsia.ch>
2020-07-12 15:18:55 +0300

    revert run_sim

--- docker/run_simulator.sh
+++ docker/run_simulator.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+# Usage:  bash run_simulator.sh <output folder> instance-id agents
+# example: bash run_simulator.sh /home/eduardo/stasp/tests L10-R1-1 n4t2-1
+
+SHAREDVOL=$1
 docker run -it  --rm \
        --env="DISPLAY" \
        --env="QT_X11_NO_MITSHM=1" \
        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-       -v /home/eduardo/swarmix_experiments:/swarmix_experiments \
+       -v ${SHAREDVOL} \
        --privileged \
        --net="host"\
-       eduardo:swarmix-full \
-       /launch/simulator ${@:1}
-
-#amd64/ubuntu:xenial \
-#       -v /opt/ibm/ILOG/CPLEX_Studio126:/cplex \
+       eduardofeo:swarmix-full \
+       /launch/simulator ${@:2} -ep ${SHAREDVOL}
