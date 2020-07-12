17eedeb57e579f161a9992dd91c8d998c6451218
Eduardo Feo <eduardo@idsia.ch>
2020-07-12 15:07:11 +0300

    updated sim scrip

--- docker/run_simulator.sh
+++ docker/run_simulator.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-# Usage:  bash run_simulator.sh <output folder> instance-id agents
-# example: bash run_simulator.sh /home/eduardo/stasp/tests L10-R1-1 n4t2-1
-
-SHAREDVOL=$1
 docker run -it  --rm \
        --env="DISPLAY" \
        --env="QT_X11_NO_MITSHM=1" \
        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-       -v ${SHAREDVOL} \
+       -v /home/eduardo/swarmix_experiments:/swarmix_experiments \
        --privileged \
        --net="host"\
-       eduardofeo:swarmix-full \
-       /launch/simulator ${@:2} -ep ${SHAREDVOL}
+       eduardo:swarmix-full \
+       /launch/simulator ${@:1}
+
+#amd64/ubuntu:xenial \
+#       -v /opt/ibm/ILOG/CPLEX_Studio126:/cplex \
